9577601c0816b4bf33d3d3489b51f6089c5e33de
Raymond Tang <fahao.tang@hotmail.com>
2024-05-20 20:56:43 +1000

    Added logo and settings for theme

---
+++ src/Kontext.AI.ClientApp/Components/SettingsPanel.razor.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Kontext.AI.ClientApp.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace Kontext.AI.ClientApp.Components;
+
+public partial class SettingsPanel
+{
+    private const string DefaultTheme = "Kontext-Next";
+    private static readonly Dictionary<string, string> ThemeColors = new(){
+        { "Kontext-Teal", "#008080" },
+        { DefaultTheme, "#20c997" },
+    };
+    
+    private static readonly string[] Colors = [.. ThemeColors.Values];
+    private string? _status;
+    private bool _ltr = true;
+    private FluentDesignTheme? _theme;
+
+    [Inject]
+    public required ILogger<SettingsPanel> Logger { get; set; }
+
+    [Inject]
+    public required CacheStorageAccessor CacheStorageAccessor { get; set; }
+
+    [Inject]
+    public required GlobalState GlobalState { get; set; }
+
+    public DesignThemeModes Mode { get; set; }
+
+    public string? SelectedColor { get; set; }
+
+    public LocalizationDirection? Direction { get; set; }
+
+    private static IEnumerable<DesignThemeModes> AllModes => Enum.GetValues<DesignThemeModes>();
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+        {
+            Direction = GlobalState.Dir;
+            _ltr = !Direction.HasValue || Direction.Value == LocalizationDirection.LeftToRight;
+        }
+    }
+
+    private static string GetColorName(string? color)
+    {
+        return ThemeColors.FirstOrDefault(x => x.Value == color).Key;
+    }
+
+    protected void HandleDirectionChanged(bool isLeftToRight)
+    {
+        _ltr = isLeftToRight;
+        Direction = isLeftToRight ? LocalizationDirection.LeftToRight : LocalizationDirection.RightToLeft;
+    }
+
+    private async Task ResetSiteAsync()
+    {
+        var msg = "Site settings reset and cache cleared!";
+
+        await CacheStorageAccessor.RemoveAllAsync();
+        _theme?.ClearLocalStorageAsync();
+
+        Logger.LogInformation(msg);
+        _status = msg;
+
+        Mode = DesignThemeModes.System;
+        SelectedColor = ThemeColors[DefaultTheme];
+    }
+}
--- src/Kontext.AI.ClientApp/Program.cs
+++ src/Kontext.AI.ClientApp/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using Kontext.AI.ClientApp;
+using Kontext.AI.ClientApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +10,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<CacheStorageAccessor>();
+// builder.Logging.AddConsole();
+
 builder.Services.AddFluentUIComponents();
 
 await builder.Build().RunAsync();
