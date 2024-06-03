e7f3e09a02a72429da8c6ec56764b4cb6d1eb147
Raymond Tang <fahao.tang@hotmail.com>
2024-06-03 23:48:02 +1000

    Added cursor to tabs

--- src/frontend/client-app/pages/settings.vue
+++ src/frontend/client-app/pages/settings.vue
@@ -2,12 +2,13 @@
     <NavList nav="tabs" tablist margin="b-3">
         <NavItem>
             <NavLink :id="'navtab-' + generalTabId" active toggle="tab" :tab="'#' + generalTabId"
-                class="d-flex align-items-center">
+                class="d-flex align-items-center cursor-pointer">
                 <Icon name="material-symbols:settings" size="20" /><span class="ms-1">General</span>
             </NavLink>
         </NavItem>
         <NavItem>
-            <NavLink :id="'navtab-' + llmsTabId" toggle="tab" :tab="'#' + llmsTabId" class="d-flex align-items-center">
+            <NavLink :id="'navtab-' + llmsTabId" toggle="tab" :tab="'#' + llmsTabId"
+                class="d-flex align-items-center cursor-pointer">
                 <Icon name="material-symbols:neurology-outline" size="20" /><span class="ms-1">LLMs</span>
             </NavLink>
         </NavItem>
