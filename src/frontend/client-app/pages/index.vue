07bd83cc296b73ec3248d4d2c3579d9335f14587
Raymond Tang <fahao.tang@hotmail.com>
2024-06-04 21:54:18 +1000

    Added card to create new chat

--- src/frontend/client-app/pages/index.vue
+++ src/frontend/client-app/pages/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <b-button button="primary" :id="btnId">Primary Button</b-button>
+  <h4>Welcome!</h4>
+  <div class="col-md-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">
+          Create a chat
+        </h5>
+        <p class="text-muted">
+          Initiate a chat with LLMs.
+        </p>
+        <NuxtLink to="/chat" class="btn btn-outline-primary">
+          <Icon name="material-symbols:chat-add-on-outline" /> <span class="ms-1">New chat</span>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
