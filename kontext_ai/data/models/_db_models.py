3b18a5d02d18fac001b407bee2defee772356f3c
Raymond Tang <fahao.tang@hotmail.com>
2024-07-20 22:06:11 +1000

    feat: Added data source table into sql model.

--- kontext_ai/data/models/_db_models.py
+++ kontext_ai/data/models/_db_models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String
+from enum import Enum
+from sqlalchemy import Boolean, Column, Integer, String, Enum as SQLEnum
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -8,3 +9,24 @@ class Setting(Base):
     __tablename__ = "settings"
     key = Column(String, primary_key=True)
     value = Column(String)
+
+
+class DataSourceType(Enum):
+    SQLite = "SQLite"
+    # DuckDB = "DuckDB"
+    # PostgreSQL = "PostgreSQL"
+    # MySQL = "MySQL"
+    # SQLServer = "SQLServer"
+    # Oracle = "Oracle"
+    # MongoDB = "MongoDB"
+    # Redis = "Redis"
+
+
+class DataSource(Base):
+    __tablename__ = "data_sources"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    description = Column(String)
+    type = Column(SQLEnum(DataSourceType))
+    conn_str = Column(String)
+    conn_str_encrypted = Column(Boolean, default=False)
