b6c5957351f6cadd4681194c303f2dcce879dceb
Raymond Tang <fahao.tang@hotmail.com>
2024-08-03 10:07:53 +1000

    refactor: move services setup to a composable to simplify app.vue

---
+++ ui/composables/useAppServices.ts
@@ -0,0 +1,91 @@
+import _ from "lodash"
+import {
+   DataProviderService,
+   DataSourceService,
+   PromptService,
+   SettingService
+} from "~/services/ApiServices"
+import type { Settings, SettingsWrapper } from "~/types/Schemas"
+import LlmService from "~/services/LlmService"
+
+const settingsWrapper = reactive<SettingsWrapper>({
+   isLoading: false,
+   loaded: false,
+   error: null,
+   settings: {} as Settings
+})
+
+export default function useAppServices() {
+   const appConfig = useAppConfig()
+
+   const settingService = new SettingService(appConfig.apiBaseUrl)
+   const promptService = new PromptService(appConfig.apiBaseUrl)
+   const dataSourceService = new DataSourceService(appConfig.apiBaseUrl)
+   const dataProviderService = new DataProviderService(appConfig.apiBaseUrl)
+   const llmService = ref<LlmService | null>(null)
+   const settings = computed(() => settingsWrapper.settings)
+
+   onMounted(() => {
+      // Fetch settings on load
+      fetchSettings()
+   })
+
+   // Function to fetch settings from the API using SettingsService
+   const fetchSettings = async () => {
+      settingsWrapper.isLoading = true
+      try {
+         settingsWrapper.settings = await settingService.getSettings()
+         llmService.value = new LlmService(
+            settingsWrapper.settings.llm_endpoint
+         )
+         settingsWrapper.error = null
+         settingsWrapper.loaded = true
+      } catch (err) {
+         settingsWrapper.error =
+            err instanceof Error ? err.message : String(err)
+      } finally {
+         settingsWrapper.isLoading = false
+      }
+   }
+
+   // Function to update a setting using SettingsService
+   const setSetting = async (key: string, value: string | number | null) => {
+      try {
+         await settingService.updateSetting(key, value)
+      } catch (err) {
+         settingsWrapper.error =
+            err instanceof Error ? err.message : String(err)
+      }
+   }
+
+   watch(
+      () => _.cloneDeep(settingsWrapper.settings),
+      async (newSettings, oldSettings) => {
+         // Determine what changed and call setSetting for those changes
+         const keys: Array<keyof Settings> = Object.keys(newSettings) as Array<
+            keyof Settings
+         >
+         // No need to update to for the initial load
+         if (Object.keys(oldSettings).length > 0)
+            for (const key of keys) {
+               if (oldSettings && newSettings[key] !== oldSettings[key]) {
+                  console.log("Updating %s=%s", key, newSettings[key])
+                  await setSetting(key, newSettings[key])
+               }
+            }
+      },
+      { deep: true }
+   )
+
+   // Register global services
+   addService("SETTINGS", settings)
+   addService("SETTING_SERVICE", settingService)
+   addService("PROMPT_SERVICE", promptService)
+   addService("DATA_SOURCE_SERVICE", dataSourceService)
+   addService("DATA_PROVIDER_SERVICE", dataProviderService)
+   addService("LLM_SERVICE", llmService)
+
+   return {
+      settingsWrapper
+   }
+}
