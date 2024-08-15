db5344fe385eeb1cbcaf0d13c1c2a6b5fd83c9df
Raymond Tang <fahao.tang@hotmail.com>
2024-08-16 08:31:46 +1000

    feat: add session and message id to chat message response

--- kontext_copilot/copilot/tools/_llm_chat_tool.py
+++ kontext_copilot/copilot/tools/_llm_chat_tool.py
@@ -4,6 +4,7 @@ from kontext_copilot import ollama
 from kontext_copilot.copilot._session import CopilotSession
 from kontext_copilot.copilot.tools._base_tool import BaseTool
 from kontext_copilot.data.schemas import ChatRequestModel, SessionMessageModel
+from kontext_copilot.data.schemas._common import ChatRoles
 from kontext_copilot.ollama._types import Message
 
 
@@ -38,6 +39,14 @@ class LlmChatTool(BaseTool):
                 Message(role=m.role, content=m.content) for m in request.messages
             ]
 
+        self.message = self.add_message(
+            message="",
+            role=ChatRoles.ASSISTANT,
+            is_streaming=request.stream or False,
+            copilot_generated=False,
+            generating=True,
+        )
+
         # Remove keys that are not part of the chat API
         params = {
             k: v for k, v in params.items() if k in client.chat.__code__.co_varnames
@@ -46,16 +55,25 @@ class LlmChatTool(BaseTool):
 
         chat_response = client.chat(**params)
 
+        # Add message to session messages table
+
         # Always return as streaming
         if isinstance(chat_response, Iterator):
 
             def generate_response():
                 for response in iter(chat_response):
                     res = _to_pydantic_model(response)
+                    res.id = self.message.id
+                    res.session_id = self.session.session_id
+                    self.append_message_part(self.message.id, res.content, res.done)
                     yield res.model_dump_json(by_alias=True) + "\n"
+                    self.append_new_line(self.message.id, True)
 
             return generate_response()
 
         if chat_response is not None:
             res = _to_pydantic_model(chat_response)
+            res.id = self.message.id
+            res.session_id = self.session.session_id
+            self.append_message_part(self.message.id, res.content, done=True)
             return res.model_dump_json(by_alias=True)
