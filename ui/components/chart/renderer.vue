ad820492dc734c3457439ccdcaa6292fbd228e67
Raymond Tang <fahao.tang@hotmail.com>
2024-09-03 22:36:55 +1000

    style: changed color list for charts

--- ui/components/chart/renderer.vue
+++ ui/components/chart/renderer.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup lang="ts">
-import type { ChartDataResponseModel } from "~/types/Schemas"
+import { ChartTypes, type ChartDataResponseModel } from "~/types/Schemas"
 import {
    Chart as ChartJS,
    Title,
@@ -47,16 +47,10 @@ const props = defineProps<{
    dataModel: ChartDataResponseModel
 }>()
 
-// const kontextColors = [
-//    "#008080",
-//    "#51a6db",
-//    "#db9430",
-//    "#20c997",
-//    "#db5c5c",
-//    "#212529"
-// ]
+const kontextColors = ["#008080", "#51a6db", "#db9430", "#20c997", "#db5c5c"]
 
 const defaultColors = [
+   ...kontextColors,
    "#3366CC",
    "#DC3912",
    "#FF9900",
@@ -81,10 +75,16 @@ const defaultColors = [
 
 const colorMode = useColorMode({ selector: "html", storageKey: "theme" })
 
-const data = computed(() => toChartJsOptions(props.dataModel, fontColor))
+const data = computed(() =>
+   toChartJsOptions(props.dataModel, fontColor, borderColor)
+)
 
 const fontColor = computed(() => {
    return colorMode.value === "dark" ? "#6c757d" : "#212529"
+})
+
+const borderColor = computed(() => {
+   return colorMode.value === "dark" ? "#adb5bd" : "#008080"
 })
 
 const emits = defineEmits(["chart-generated"])
@@ -93,7 +93,8 @@ const rendered = ref(false)
 
 const toChartJsOptions = (
    model: ChartDataResponseModel,
-   fontColor: Ref<string>
+   fontColor: Ref<string>,
+   borderColor: Ref<string>
 ): ChartRendererProps => {
    const legend = {
       display: true,
@@ -124,8 +125,20 @@ const toChartJsOptions = (
    if (!Object.hasOwn(options.plugins, "legend")) {
       options.plugins.legend = legend
    }
+   if (model.data.datasets) {
+      model.data.datasets.forEach((dataset, i) => {
+         if (!dataset.backgroundColor) {
+            if (model.type === ChartTypes.PIE)
+               dataset.backgroundColor = defaultColors
+            else dataset.backgroundColor = defaultColors[i]
+         }
+         if (!dataset.borderColor && model.type !== ChartTypes.PIE) {
+            dataset.borderColor = borderColor.value
+         }
+      })
+   }
 
-   const data = Object.assign({}, model.data)
+   const data = Object.assign(model.data)
    return {
       chartData: data,
       chartOptions: options,
